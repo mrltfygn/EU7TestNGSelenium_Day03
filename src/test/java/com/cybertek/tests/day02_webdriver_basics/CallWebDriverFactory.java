cb99e62e63b6c9f95b223ae34d5ed123c1f90b7c
mrltfygn <omeryegen88@gmail.com>
2021-12-24 21:56:35 +0100

    added a line

---
+++ src/test/java/com/cybertek/tests/day02_webdriver_basics/CallWebDriverFactory.java
@@ -0,0 +1,15 @@
+package com.cybertek.tests.day02_webdriver_basics;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class CallWebDriverFactory {
+    public static void main(String[] args) {
+        WebDriver driver= WebDriverFactory.getDriver("Chrome");
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        System.out.println("title = " + title);
+        
+    }
+}
+
