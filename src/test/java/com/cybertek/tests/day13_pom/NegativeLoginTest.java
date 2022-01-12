9fca49b23b726987aa65cb03561d9fad85866b94
mrltfygn <omeryegen88@gmail.com>
2022-01-12 13:17:51 +0100

    NegativeLoginTest

--- src/test/java/com/cybertek/tests/day13_pom/NegativeLoginTest.java
+++ src/test/java/com/cybertek/tests/day13_pom/NegativeLoginTest.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day13_pom;
 
+import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
+import org.apache.commons.logging.Log;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,11 +13,23 @@ public class NegativeLoginTest extends TestBase {
     @Test
     public void wrongPasswordTest(){
 
-
-        driver.findElement(By.id("prependedInput")).sendKeys("user1");
-        driver.findElement(By.id("prependedInput2")).sendKeys("asdagfag");
-        driver.findElement(By.id("_submit")).click();
+        LoginPage loginPage=new LoginPage();
+        loginPage.usernameInput.sendKeys("user1");
+        loginPage.loginButton.sendKeys("asdsaafa");
+        loginPage.loginButton.click();
+//        driver.findElement(By.id("prependedInput")).sendKeys("user1");
+//        driver.findElement(By.id("prependedInput2")).sendKeys("asdagfag");
+//        driver.findElement(By.id("_submit")).click();
 
         Assert.assertEquals(driver.getCurrentUrl(),"https://qa1.vytrack.com/user/login");
     }
+    @Test
+    public void wrongUsernamTest(){
+        LoginPage loginPage=new LoginPage();
+        loginPage.usernameInput.sendKeys("asggafgaga");
+        loginPage.passwordInput.sendKeys("UserUser123");
+        loginPage.passwordInput.click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://qa1.vytrack.com/user/login");
+    }
+
 }
