5c76f06840c5af5106ce3ef31cd099c7b2d711fa
mrltfygn <omeryegen88@gmail.com>
2022-01-25 00:38:10 +0100

    PageSubtitle

--- src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
+++ src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
@@ -1,4 +1,49 @@
 package com.cybertek.tests.day13_pom;
 
-public class RepeatOptionsTests {
+import com.cybertek.pages.CalendarEventsPage;
+import com.cybertek.pages.CreateCalendarEventsPage;
+import com.cybertek.pages.DashboardPage;
+import com.cybertek.pages.LoginPage;
+import com.cybertek.tests.TestBase;
+import com.cybertek.utilities.BrowserUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RepeatOptionsTests extends TestBase {
+
+     /*VERIFY RADIO BUTTONS
+    Open Chrome browser
+    Login as driver
+    Go to Activities->Calendar Events
+    Click on create calendar events
+    Click on repeat
+    Verify that repeat every days is checked
+    verify that repeat weekday is not checked
+    * */
+    @Test
+    public void test1(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsDriver();
+        DashboardPage dashboardPage=new DashboardPage();
+        dashboardPage.navigateToModule("Activities","Calendar Events");
+
+
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        //Click on create calendar events
+        //wait for loader mask
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForClickablility(calendarEventsPage.createCalendarEvent,5);
+        calendarEventsPage.createCalendarEvent.click();
+
+        CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+        // Click on repeat
+        createCalendarEventsPage.repeat.click();
+
+        //Verify that repeat every days is checked
+        Assert.assertTrue(createCalendarEventsPage.days.isSelected(),"Verify repeat days is selected");
+        //verify that repeat weekday is not checked
+        Assert.assertFalse(createCalendarEventsPage.weekday.isSelected(),"Verify weekday is not selected");
+
+    }
+
 }
