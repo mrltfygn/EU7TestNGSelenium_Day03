0ed44cf662d6d8f92ae38518673fb200809b6469
mrltfygn <omeryegen88@gmail.com>
2022-01-25 01:05:14 +0100

    RepeatOptionsTests

--- src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
+++ src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
@@ -6,8 +6,14 @@ import com.cybertek.pages.DashboardPage;
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RepeatOptionsTests extends TestBase {
 
@@ -44,6 +50,43 @@ public class RepeatOptionsTests extends TestBase {
         //verify that repeat weekday is not checked
         Assert.assertFalse(createCalendarEventsPage.weekday.isSelected(),"Verify weekday is not selected");
 
+    }
+     /*
+     VERIFY REPEAT OPTIONS
+        Open Chrome browser
+        Login as driver
+        Go to Activities->Calendar Events
+        Click on create calendar events button
+        Click on repeat checkbox
+        Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+     */
+
+    @Test
+    public void test2(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsDriver();
+        DashboardPage dashboardPage=new DashboardPage();
+        dashboardPage.navigateToModule("Activities","Calendar Events");
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        //Click on create calendar events
+        //wait for loader mask
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForClickablility(calendarEventsPage.createCalendarEvent,5);
+        calendarEventsPage.createCalendarEvent.click();
+        CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+        // Click on repeat
+        createCalendarEventsPage.repeat.click();
+
+        Select repeatDropDown=createCalendarEventsPage.repeatOptionsList();
+        //Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+        List<WebElement> actualOptions=repeatDropDown.getOptions();
+
+        List<String> actualList=new ArrayList<>();
+        List<String> expectedList= Arrays.asList("Daily","Weekly","Monthly","Yearly");
+        for(WebElement option:actualOptions){
+            actualList.add(option.getText());
+        }
+        Assert.assertEquals(actualList,expectedList,"Verify List");
     }
 
 }
