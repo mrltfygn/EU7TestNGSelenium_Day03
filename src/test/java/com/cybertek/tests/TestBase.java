24c0fe42042c2713b0f9d9c3fa2a218ee0e20a5b
mrltfygn <omeryegen88@gmail.com>
2022-01-25 21:34:45 +0100

    Testbase Updated

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -1,12 +1,17 @@
 package com.cybertek.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +19,33 @@ public class TestBase {
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
+    protected  ExtentReports report;
+    protected  ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+    @BeforeTest
+    public void setUpTest(){
+        //initialize the class
+        report = new ExtentReports();
 
+        //create a report path
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath + "/test-output/report.html";
+
+        //initialize the html reporter with the report path
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        //attach the html report to report object
+        report.attachReporter(htmlReporter);
+
+        //title in report
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+
+        //set environment information
+        report.setSystemInfo("Environment","QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS",System.getProperty("os.name"));
+
+    }
     @BeforeMethod
     public void setUp(){
         driver= Driver.get();
@@ -26,8 +57,13 @@ public class TestBase {
     }
     @AfterMethod
     public void tearDown() throws InterruptedException {
-        Thread.sleep(8000);
+        Thread.sleep(3000);
         Driver.closeDriver();
+    }
+    @AfterTest
+    public void tearDownTest(){
+        //this is when the report is actually created
+        report.flush();
     }
 }
 
